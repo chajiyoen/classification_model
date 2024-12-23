0e35e1f40f8daf2b7ecfe51884f11f46a4d7e8a1
jiyoen <chayouree@gmail.com>
2024-12-23 11:16:26 +0900

    이미지 증강 완료

---
+++ dataset.js
@@ -0,0 +1,20 @@
+//데이터 17KPamLcwRpk7yZVpjMqqaumfIBL-8R8k
+data_sets = []
+
+//메뉴 생성기 종료 E===========================================
+//데이터 아키텍처{sub_title:"",sub_content:"",sub_img:[],user_fill:""}
+data_sets.push({sub_title:"이미지크롤링",
+  sub_content:"구글 사이트 및 네이버 사이트를 이동하면서 해당 검색어의 이미지를 자동 수집",
+  sub_img:["1sDAc83_d8JZzwhfZN4rw2QMfLsVlCyKS","17R9LczgSEDxTJNT1VYMbMM7ErDXV8JUF",
+    "1VXMWs-FPDuKKuQYr6nql2H6rvHdkpeN_","1G738ptp25MElnhRexrCb93AKY808VnC2"
+  ],
+  user_fill:`최적의 검증데이터 적확도를 찾기 위해 조기종료 콜백함수와 adam의 최적화 함수`,
+  asso_file:"관련파일 /Trainning/construct_model 호출 실행"
+})
+data_sets.push({sub_title:"훈련된 모델의 평가 및 예측",
+    sub_content:"구성된 모델과 전처리된 데이터를 이용하여 모델의 훈련",
+    sub_img:["1sDAc83_d8JZzwhfZN4rw2QMfLsVlCyKS","17R9LczgSEDxTJNT1VYMbMM7ErDXV8JUF",
+    "1VXMWs-FPDuKKuQYr6nql2H6rvHdkpeN_","1G738ptp25MElnhRexrCb93AKY808VnC2"],
+    user_fill:`최적의 검증데이터 정확도를 찾기위해 조기종료 콜백함수와  adam의 최적화 함수`,
+    asso_file:"관련파일 / Trainning/construct_model 호출 실행"
+})
