10190bc977ef902f1ced2da9dea25c4b045b2fed
chajiyoen <chayouree@gmail.com>
2024-12-27 10:34:35 +0900

    Update common_menu.js

--- common_menu.js
+++ common_menu.js
@@ -10,8 +10,8 @@ class Menu {
 menu1= new Menu("CNN AND Crawling");
 menu1.url="https://github.com/chajiyoen/classification_model/"
 menu1.tips="네이버, 구글 이미지 크롤링 및 컨볼루션 적용한 모델";
-menu2 = new Menu("test menu")
-menu2.url="127.0.0.1"
-menu2.tips= "테스트용 메뉴"
+menu2 = new Menu("RNN AND LSTM encryto meney")
+menu2.url="https://chajiyoen.github.io/RNN_crytoanal/"
+menu2.tips= "가상화페 분석을 이용한 미래 가격 측정"
 menu_sets.push(menu1)
 menu_sets.push(menu2)
